5a7c87a4d098fd8ea9e78efdd5bdbaf6c44cd7e3
Robert Skelton <robertjskelton@gmail.com>
2018-05-18 09:05:55 -0400

    tomcat service file

--- recipes/tomcat.rb
+++ recipes/tomcat.rb
@@ -1,38 +1,21 @@
-# # Manual Installation Instructions for Tomcat
-#
-# Install OpenJDK 7 JDK using yum, run this command:
-#
-# ```shell
-# $ sudo yum install java-1.7.0-openjdk-devel
-# ```
+# recipe: tomcat
+
+# Install OpenJDK 7 JDK
 package 'java-1.7.0-openjdk-devel'
 
-#
-# Create a user for tomcat
-#
-# ```
-# $ sudo groupadd tomcat
-# $ sudo useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
-# ``
+# tomcat user
 user 'tomcat' do
   comment 'tomcat user'
   home '/opt/tomcat'
 end
 
+# tomcat group
 group 'tomcat' do
   action :modify
   members 'tomcat'
 end
 
-# Download the Tomcat Binary
-#
-# > NOTE: A specific binary will be mentioned below but it will likely be out of date. You can find the binaries for Tomcat 8 here at https://archive.apache.org/dist/tomcat/tomcat-8/
-#
-# ```
-# $ cd /tmp
-# $ wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz
-# ```
-
+# download tomcat from source
 remote_file '/tmp/apache-tomcat-8.5.20.tar.gz' do
   source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.52/bin/apache-tomcat-8.0.52.tar.gz'
   owner 'tomcat'
@@ -42,11 +25,7 @@ remote_file '/tmp/apache-tomcat-8.5.20.tar.gz' do
 end
 
 
-# Extract the Tomcat Binary
-#
-# ```
-# $ sudo mkdir /opt/tomcat
-directory '/opt/tomcat' do
+# create tomcat home directory
   owner 'tomcat'
   group 'tomcat'
   mode '0755'
@@ -67,40 +46,16 @@ end
 # Install the Systemd Unit File
 #
 # ```
-# $ sudo vi /etc/systemd/system/tomcat.service
-# ```
-#
-# This is the content of that file:
-#
-# ```
-# # Systemd unit file for tomcat
-# [Unit]
-# Description=Apache Tomcat Web Application Container
-# After=syslog.target network.target
-#
-# [Service]
-# Type=forking
-#
-# Environment=JAVA_HOME=/usr/lib/jvm/jre
-# Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-# Environment=CATALINA_HOME=/opt/tomcat
-# Environment=CATALINA_BASE=/opt/tomcat
-# Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-# Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-#
-# ExecStart=/opt/tomcat/bin/startup.sh
-# ExecStop=/bin/kill -15 $MAINPID
-#
-# User=tomcat
-# Group=tomcat
-# UMask=0007
-# RestartSec=10
-# Restart=always
-#
-# [Install]
-# WantedBy=multi-user.target
-# ```
-#
+
+# move files/tomcat.service into place
+cookbook_file '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service'
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0755'
+  action :create
+end
+
 # Reload Systemd to load the Tomcat Unit file
 #
 # ```
