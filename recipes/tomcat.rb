45afc63f7f98904dde744ba8e0fc593802eeec98
Robert Skelton <robertjskelton@gmail.com>
2018-05-18 09:15:06 -0400

    hard code directories

--- recipes/tomcat.rb
+++ recipes/tomcat.rb
@@ -49,9 +49,9 @@ bash 'update_permissions' do
   cwd ::File.dirname('/tmp')
   code <<-EOH
       sudo chgrp -R tomcat /opt/tomcat
-      sudo chmod -R g+r conf
-      sudo chmod g+x conf
-      sudo chown -R tomcat webapps/ work/ temp/ logs/
+      sudo chmod -R g+r /opt/tomcat/conf
+      sudo chmod g+x /opt/tomcat/conf
+      sudo chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/
     EOH
 end
 
