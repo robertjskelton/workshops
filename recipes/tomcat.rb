f2d10363e17f6cc8257a286ca9e1f98b1a6252d0
Robert Skelton <robertjskelton@gmail.com>
2018-05-18 09:13:03 -0400

    bash block

--- recipes/tomcat.rb
+++ recipes/tomcat.rb
@@ -44,7 +44,18 @@ end
 #
 # Update the Permissions
 #
-# ```
+# update permissions, running this in bash because lots of commands
+bash 'update_permissions' do
+  cwd ::File.dirname(/tmp)
+  code <<-EOH
+      sudo chgrp -R tomcat /opt/tomcat
+      sudo chmod -R g+r conf
+      sudo chmod g+x conf
+      sudo chown -R tomcat webapps/ work/ temp/ logs/
+    EOH
+  not_if { ::File.exist?(extract_path) }
+end
+
 # $ sudo chgrp -R tomcat /opt/tomcat
 # $ sudo chmod -R g+r conf
 # $ sudo chmod g+x conf
